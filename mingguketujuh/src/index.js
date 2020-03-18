332e1c41c3e8c48c4ed614be49873cf1ce8ebec1
bamzdsptd <bambangdnr09@gmail.com>
2020-03-18 13:27:09 +0700

    update bootstrap dan css

--- mingguketujuh/src/index.js
+++ mingguketujuh/src/index.js
@@ -7,6 +7,7 @@ import { Provider } from 'react-redux'
 import { createStore, applyMiddleware, compose } from 'redux' 
 import MainReducer from './reducers/MainReducer'  
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 // import registerServiceWorker from './registerServiceWorker'; 
  
