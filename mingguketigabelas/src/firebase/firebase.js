d0b11bbbd777b2e9a17b568839dce30372ee5149
bamzdsptd <bambangdnr09@gmail.com>
2020-05-01 20:07:29 +0700

    MINGGU 13

---
+++ mingguketigabelas/src/firebase/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBodsEs6LOG2JuCI6hbKr9WDtMDychZ5Z8",
+  authDomain: "minggukeduabelas-pbf.firebaseapp.com",
+  databaseURL: "https://minggukeduabelas-pbf.firebaseio.com",
+  projectId: "minggukeduabelas-pbf",
+  storageBucket: "minggukeduabelas-pbf.appspot.com",
+  messagingSenderId: "166929797429",
+  appId: "1:166929797429:web:67d80e39d63aad28463c7c"
+};
+
+export const myFirebase = firebase.initializeApp(firebaseConfig);
+const baseDb = myFirebase.firestore();
+export const db = baseDb;
---
+++ mingguketigabelas/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+
+import BlogPost from "./container/BlogPost";
+
+ReactDOM.render(<BlogPost />, document.getElementById('root'));
+
+serviceWorker.unregister();
+
